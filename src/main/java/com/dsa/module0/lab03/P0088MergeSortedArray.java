32834863ae31c01d632d455184dbda77abfca885
toobidu <dungtien3kk@gmail.com>
2026-09-13 17:09:22 +0700

    refactor: Merge Sorted Array Three Pointers Implementation

--- src/main/java/com/dsa/module0/lab03/P0088MergeSortedArray.java
+++ src/main/java/com/dsa/module0/lab03/P0088MergeSortedArray.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
  * Điền ba dòng này TRƯỚC khi gõ dòng code đầu tiên:
  * Ràng buộc và biên : nums1.length == m + n; nums2.length == n; 0 <= m, n <= 200; 1 <= m + n <= 200; -10^9 <= nums1[i], nums2[j] <= 10^9
  * Brute force + Big-O: sao chép trc tiếp tất cả các phần tử của nums 2 vào phần đuôi đệm rồi sắp xếp lại; O(m+n)log(m+n)
- * Hướng tối ưu       :
+ * Hướng tối ưu       : Duyệt ngược 3 con trỏ (Three Pointers) từ đuôi m+n-1, so sánh và điền phần tử lớn hơn vào cuối mảng nums1 để tránh ghi đè dữ liệu. Time O(m+n), Space O(1).
  * Điền sau khi pass:
  * bruteForce Time: O((m+n)log(m+n))  Space: O(1)
  * optimal    Time: O(m+n)            Space: O(1)
@@ -20,36 +20,33 @@ import java.util.Arrays;
 public class P0088MergeSortedArray {
 
     public void bruteForce(int[] nums1, int m, int[] nums2, int n) {
-        for (int i = 0; i < n; i++){
-            nums1[m + i] = nums2[i];
-        }
-        Arrays.sort(nums1);
+       for(int i = 0;  i < n; i++){
+           nums1[m + i] = nums2[i];
+       }
+       Arrays.sort(nums1);
     }
 
 
     public void optimal(int[] nums1, int m, int[] nums2, int n) {
-        int lastIndexNums1ByInt = m - 1;
-        int lastIndexNums2ByInt = n - 1;
-        int lastIndexNums1 = m + n - 1;
-        while(lastIndexNums1ByInt >= 0 && lastIndexNums2ByInt >= 0){
-            if (nums1[lastIndexNums1ByInt] > nums2[lastIndexNums2ByInt]){
-                nums1[lastIndexNums1] = nums1[lastIndexNums1ByInt];
-                lastIndexNums1ByInt--;
+        int a = m - 1;
+        int b = n - 1;
+        int c = m + n - 1;
+        while(a >= 0 && b >= 0){
+            if(nums1[a] > nums2[b]){
+                nums1[c] = nums1[a];
+                a--;
             }else{
-                nums1[lastIndexNums1] = nums2[lastIndexNums2ByInt];
-                lastIndexNums2ByInt--;
+                nums1[c] = nums2[b];
+                b--;
             }
-            lastIndexNums1--;
+            c--;
         }
 
-        while (lastIndexNums2ByInt >= 0){
-            nums1[lastIndexNums1] = nums2[lastIndexNums2ByInt];
-            lastIndexNums2ByInt--;
-            lastIndexNums1--;
+        while(b >= 0){
+            nums1[c] = nums2[b];
+            b--;
+            c--;
         }
     }
 
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        optimal(nums1, m, nums2, n);
-    }
 }
