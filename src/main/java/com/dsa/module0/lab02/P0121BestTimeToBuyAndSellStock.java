4db9580fd1071ce889ba2335f7d28b053e5ccd24
toobidu <dungtien3kk@gmail.com>
2026-09-13 17:01:48 +0700

    refactor: Best Time To Buy And Sell Stock Variable Cleanup

--- src/main/java/com/dsa/module0/lab02/P0121BestTimeToBuyAndSellStock.java
+++ src/main/java/com/dsa/module0/lab02/P0121BestTimeToBuyAndSellStock.java
@@ -22,11 +22,12 @@ public class P0121BestTimeToBuyAndSellStock {
         int maxProfit = 0;
         for (int i = 0; i < prices.length - 1; i++) {
             for (int j = i + 1; j < prices.length; j++) {
-                int profit = prices[j] - prices[i];
-                if (profit > maxProfit) {
-                    maxProfit = profit;
+                int sub = prices[j] - prices[i];
+                if (sub > maxProfit) {
+                    maxProfit = sub;
                 }
             }
+//            return maxProfit;
         }
         return maxProfit;
     }
