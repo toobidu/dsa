0f04419de10a4c872a3ff47929e219b5bcceca3f
toobidu <dungtien3kk@gmail.com>
2026-09-13 17:00:51 +0700

    refactor: Two Sum Variable Naming And Loop Bounds

--- src/main/java/com/dsa/module0/lab01/P0001TwoSum.java
+++ src/main/java/com/dsa/module0/lab01/P0001TwoSum.java
@@ -22,21 +22,21 @@ import java.util.Map;
 public class P0001TwoSum {
 
     public int[] bruteForce(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) return new int[]{i, j};
+        for(int i = 0; i < nums.length - 1; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if (nums[i] + nums[j] == target) return new int[] {i, j};
             }
         }
-        return new int[]{};
+        return new int[] {};
     }
 
     public int[] optimal(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
+        Map<Integer, Integer> numsMap = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
             int result = target - nums[i];
-            if (map.containsKey(result)) return new int[]{map.get(result), i};
-            map.put(nums[i], i);
+            if(numsMap.containsKey(result)) return new int[] {numsMap.get(result), i};
+            numsMap.put(nums[i], i);
         }
-        return new int[]{};
+        return new int[] {};
     }
 }
